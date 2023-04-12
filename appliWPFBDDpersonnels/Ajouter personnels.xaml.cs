173ae3caf03b43068d431018009e720851bb831e
Quentin Dida-Juhel <quentindj14@gmail.com>
2023-04-12 17:20:25 +0200

    Exemple a modifier et tester si compatible

--- appliWPFBDDpersonnels/Ajouter personnels.xaml.cs
+++ appliWPFBDDpersonnels/Ajouter personnels.xaml.cs
@@ -93,8 +93,17 @@ namespace appliWPFBDDpersonnels
                 TNom.Text = ((Personnel)(sender as DataGrid).SelectedItem).Nom;
                 TPrénom.Text = ((Personnel)(sender as DataGrid).SelectedItem).Prenom;
                 IMGpersonnels.Source = CBDDpersonnels.LoadImage( ((Personnel)(sender as DataGrid).SelectedItem).Photo);
-                
-                
+
+                //exemple a modifier si exact
+              //  private void createGraphicsColumn()
+             //   {
+                //    Icon treeIcon = new Icon(this.GetType(), "tree.ico");
+                 //   DataGridViewImageColumn iconColumn = new DataGridViewImageColumn();
+                 //   iconColumn.Image = treeIcon.ToBitmap();
+                 //   iconColumn.Name = "Tree";
+                   // iconColumn.HeaderText = "Nice tree";
+                  //  dataGridView1.Columns.Insert(2, iconColumn);
+              //  }
             }
         }
     }
